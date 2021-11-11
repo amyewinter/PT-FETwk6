d65ff484e4af5c8793f64967b3f56e13cf29f6d2
Amy Winter <amywinter@unm.edu>
2021-11-11 11:39:47 -0700

    Shop built

--- index.js
+++ index.js
@@ -18,3 +18,68 @@ Customers are ignored for now.
 Items will have properties itemName, itemNum, type, status, and price.
 
 */
+
+class Shop {
+  constructor(shopName) {
+    this.shopName = shopName;
+    this.items = [];
+    this.owners = [];
+  }
+
+  addItem(itemName, itemNum, type, status, price) {
+    this.items.push(new Item(itemName, itemNum, type, status, price));
+  }
+
+  addOwner(ownerName) {
+    this.owners.push(new Owner(ownerName));
+  }
+}
+
+class Item {
+  constructor(itemName, itemNum, type, status, price) {
+    this.itemName = itemName;
+    this.itemNum = itemNum;
+    this.type = type;
+    this.status = status;
+    this.price = price;
+  }
+}
+
+class Owner {
+  constructor(ownerName) {
+    this.ownerName = ownerName;
+  }
+}
+
+class ShopService {
+  static url = "";
+
+  static getAllShops() {
+    return $.get(this.url);
+  }
+
+  static getShop(id) {
+    return $.get(this.url + `/${id}`);
+  }
+
+  static createShop(shopName) {
+    return $.post(this.url, shopName);
+  }
+
+  static updateShop(shopName) {
+    return $.ajax({
+      url: this.url + `/${shopName._id}`,
+      dataType: "json",
+      data: JSON.stringify(shopName),
+      contentType: "application/json",
+      type: "PUT",
+    });
+  }
+
+  static deleteShop(id) {
+    return $.ajax({
+      url: this.url + `/${id}`,
+      type: "DELETE",
+    });
+  }
+}
