31ed127d60a552ae09ac84fcd1e93711d0ec23ae
Amy Winter <amywinter@unm.edu>
2021-11-11 10:36:24 -0700

    Add initial versions of required files

---
+++ index.js
@@ -0,0 +1,20 @@
+/*  Final coding project, week 6, Front End Technologies
+Build a create-read-update-delete application using a remote API https://crudcrud.com/
+
+This is a prototype of an item tracking system for a small-scale online store similar to Etsy etc.  The business model is  a shop run by my friend, Desert Oddities; she purchases southwestern-style jewelry and other items at thrift stores, refurbishes them, and sells them online.
+
+https://www.etsy.com/shop/DesertOddities
+
+Possible objects are shop, owner, customer, and item.
+
+This prototype will track shops and items.
+
+A shop will have at least one owner and multiple items (arrays).  It will have only one property, shopName.
+
+Owners will have only one property as well, ownerName.
+
+Customers are ignored for now.
+
+Items will have properties itemName, itemNum, type, status, and price.
+
+*/
